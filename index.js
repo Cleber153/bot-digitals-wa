bf68739a2f5438a4ceae66ea5a604a267d26484d
Cleber153 <81826813+Cleber153@users.noreply.github.com>
2026-03-04 06:24:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
+        executablePath: '/usr/bin/google-chrome-stable' // Esto es clave en Docker
     }
 });
 
